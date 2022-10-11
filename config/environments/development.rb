dd5537d9b045f94c0a762430987273d11a9d9c66
VuDej <drvu47@gmail.com>
2022-10-11 10:37:21 +0200

    fix url mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = {:host => 'https://app-restaurants-dejan.herokuapp.com/', :protocol => 'http'}
+  config.action_mailer.default_url_options = {:host => 'https://restaurants-dejan.herokuapp.com/', :protocol => 'http'}
   config.action_mailer.perform_deliveries = true
 
   config.action_mailer.delivery_method = :smtp
