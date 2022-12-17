cf8629c027b85c43ac1b37625f6de806936c1a86
VuDej <drvu47@gmail.com>
2022-12-17 12:03:00 +0100

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  https://restaurants-app-dejan.up.railway.app/
+  config.hosts << "restaurants-app-dejan.up.railway.app"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
